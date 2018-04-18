c7852c8a88560afc0a824021c65ae267b7252514
Johnny Huynh <johnnydcs@yahoo.com>
2018-04-17 23:27:54 -0700

    .

--- src/TankGame/Window.java
+++ src/TankGame/Window.java
@@ -1,7 +1,25 @@
 package TankGame;
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
 
-public class Window {
+public class Window extends JPanel {
 
+	private int nTiles = 12;
+	private ImageIcon[] theBackground;
+	
+	public Window() {
+		
+		loadImage();
+	}
+	
+	private ImageIcon loadImage() {
+	
+		theBackground = new ImageIcon[nTiles];
+		return theBackground;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
