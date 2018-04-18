d383eff00fb9a14daa22cf8fd3e1ccb319e416fe
Adan Guanlao <aguanlaojr@gmail.com>
2018-04-18 16:20:10 -0700

    Testing

--- src/TankGame/Window.java
+++ src/TankGame/Window.java
@@ -2,17 +2,52 @@ package TankGame;
 
 import java.util.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+//import java.io.File;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class Window extends JFrame implements Observer{
         private static final int WINDOW_WIDTH = 1200;
         private static final int WINDOW_HEIGHT = 800;
+        
         private final JFrame mainWindow;        
-	
+	private BufferedImage background;
+        
 	public Window() {
             mainWindow = new JFrame("Tanks");
             initializeFrame();
-	}
+            
+            File test = new File("resources/background_tile.png");
+            
+            System.out.println("CWD: " + System.getProperty("user.dir"));
+            try {
+                background = ImageIO.read(test);
+            }   catch   (IOException e) {
+                System.out.println("Error reading background file.");
+//            }
+//            JLabel label = new JLabel(new ImageIcon(background));
+//            mainWindow.add(label);
+//            mainWindow.repaint();
+//            
+    
+            }
+            JLabel label = new JLabel(new ImageIcon(background));
+            label.setPreferredSize(new Dimension(100,100));
+            mainWindow.getContentPane().add(label);
+            mainWindow.repaint();
+            
+        }
+        
+        @Override
+        public void paint(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g;
+            System.out.println("Drawing image.");
+            
+            g2.drawImage(background, 300, 300, null);
+            g2.drawString("Testing drawing", 100, 50);
+        }
         
         private void initializeFrame() {
             mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,7 +66,7 @@ public class Window extends JFrame implements Observer{
         @Override
         public void update(Observable observed, Object arg) {
             //On clock tick, redraw screen
-            System.out.println("Window saw clock tick.");
+            //System.out.println("Window saw clock tick.");
         }
 	
 
