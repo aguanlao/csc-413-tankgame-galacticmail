9c4498ccf03baeb68b65eb00a5f7d9833382ba1b
Adan Guanlao <aguanlaojr@gmail.com>
2018-05-11 17:18:47 -0700

    Drawing background image to screen

--- src/galacticmail/GalacticPanel.java
+++ src/galacticmail/GalacticPanel.java
@@ -6,23 +6,35 @@ import java.util.List;
 import java.util.ArrayList;
 import javax.swing.*;
 import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 import common.GameObject;
 
 public class GalacticPanel extends JPanel{
-    private static final int VIEW_WIDTH = 800;
+    private static final int VIEW_WIDTH = 1200;
     private static final int VIEW_HEIGHT = 800;
+    private static final String BACKGROUND_IMAGE = "galacticmail" + File.separator 
+            + "resources" + File.separator + "Background.png";
     
     private final GalacticWorld world;
+    private BufferedImage background;
     
     Asteroid a;
     
     public GalacticPanel(GalacticWorld world) {
+        int width = GalacticWorld.WORLD_WIDTH;
+        int height = GalacticWorld.WORLD_HEIGHT;
+        
         this.world = world;
+        try {
+            background = ImageIO.read(new File(BACKGROUND_IMAGE));
+        } catch (IOException exception) {
+            System.err.println("Error reading background file.");
+            exception.printStackTrace();
+        }
+        
         a = new Asteroid(50, 50);
-    }
-    
-    private void createBackground() {
     }
     
     public void redrawWorldImage() {
@@ -32,6 +44,7 @@ public class GalacticPanel extends JPanel{
     @Override
     public void paintComponent(Graphics graphics) {
         Graphics2D g2D = (Graphics2D)graphics.create();
+        graphics.drawImage(background, 0, 0, VIEW_WIDTH, VIEW_HEIGHT, null);
         for(int i = 0; i < world.getAsteroids().size(); i++) {
             world.getAsteroids().get(i).repaint(g2D);
         }
